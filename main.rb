e76bcce179112085a786ffbb333433191f0c2f70
James Wu <ichangjwu@outlook.com>
2021-12-23 15:18:20 +0900

    Define #build_tree and add test array

--- main.rb
+++ main.rb
@@ -1,2 +1,6 @@
 require_relative 'node.rb'
 require_relative 'tree.rb'
+
+example = [749, 375, 289, 458, 253, 305, 189, 320, 548, 484, 172, 819, 406, 27, 839, 31, 198, 716, 93, 399, 222, 532, 943, 294, 363, 578, 475, 317]
+tree = Tree.new(example)
+tree.pretty_print
--- tree.rb
+++ tree.rb
@@ -6,4 +6,25 @@ class Tree
     @array = array
     @root = build_tree
   end
+
+  def build_tree
+    self.array = array.sort.uniq
+    create_BST(array)
+  end
+
+  def create_BST(ary, first = 0, last = ary.size - 1)
+    return if first > last
+
+    mid = (first + last) / 2
+    node = Node.new(ary[mid])
+    node.left = create_BST(ary, first, mid - 1)
+    node.right = create_BST(ary, mid + 1, last)
+    node
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
