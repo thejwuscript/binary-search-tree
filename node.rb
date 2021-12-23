7dd1f243459495c70c8d0a0410fd1b09d7cdb7d7
James Wu <ichangjwu@outlook.com>
2021-12-24 02:53:54 +0900

    Define the <=> operator in class Node

--- node.rb
+++ node.rb
@@ -2,10 +2,16 @@ class Node
   include Comparable
 
   attr_accessor :left, :right, :data
-  
+
   def initialize(data)
     @data = data
     @left = nil
     @right = nil
+  end
+
+  def <=>(other)
+    return data <=> other if other.is_a? Integer
+
+    data <=> other.data
   end
 end
